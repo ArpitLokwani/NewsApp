ca22069db856c67d99820af39023b66504f08bf6
bizofitmobile <info@bizofit.com>
2020-06-27 13:06:23 +0530

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Arpit Lokwani on 27/06/20.
+//  Copyright © 2020 Arpit Lokwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
